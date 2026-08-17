b944d043f83b356ee85b1cfc7bf21fc3b1660f64
Ankit Kumar <jaikumar9760@gmail.com>
2026-08-17 16:46:46 +0530

    Update DashboardView.tsx

--- src/components/pages/DashboardView.tsx
+++ src/components/pages/DashboardView.tsx
@@ -7,7 +7,7 @@ import { Throughput } from '../Throughput';
 import { DigitalTwin } from '../DigitalTwin';
 import { ThermalCamera } from '../ThermalCamera';
 
-const API_BASE = 'https://api.axionsystems.de';
+const API_BASE = 'http://axion-telemetry-query-svc.default.svc.cluster.local:8000';
 
 interface DashboardViewProps {
   devices: any[];
