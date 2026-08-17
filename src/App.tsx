a198827259641c9b7cde429097406a9932332ad4
Ankit Kumar <jaikumar9760@gmail.com>
2026-08-17 18:38:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { SystemSettings } from './components/pages/SystemSettings';
 import { DashboardView } from './components/pages/DashboardView';
 import { NotFound } from './components/pages/NotFound';
 
-const API_BASE = 'http://10.0.5.150:8000';
+const API_BASE = 'http://4.187.182.59:8000';
 
 function App() {
   const location = useLocation();
